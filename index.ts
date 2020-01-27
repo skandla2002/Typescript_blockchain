87a54715e9b50d16f2474a4adbced5b11194ef4b
JONGMEONG PAK <skandla2002@gmail.com>
2020-01-28 00:08:12 +0900

    소스 커밋

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 const name = "Nicolas",
-age = 24,
-gender = "male"
+        age = 24,
+        gender = "male";
+
+const sayHi = (name: string, age:number, gender:string):string => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+console.log(sayHi("Nicolas", 24, "male"));
+
+export {};
